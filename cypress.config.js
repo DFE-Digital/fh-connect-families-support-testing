5fc916fbb630d467c13bbed8f649b7599c1ebbe6
MDola <madhu.dola@digital.education.gov.uk>
2022-10-05 12:14:39 +0100

    Fix spec names

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      'baseUrl': 'http://localhost:21914/',
+      // 'baseUrl': 'http://localhost:21914/',
     //  'baseUrl': 'https://localhost:7216/',
-
+    'baseUrl': 'https://as-fh-service-directory-admin-ui-main.azurewebsites.net/',
   //  'baseUrl': 'https://fh-family-experience-prototyp.herokuapp.com/',
    //'baseUrl': 'http://localhost:3000/mvp-1/find-services-group-or-activity', 
     'specPattern': ['**/*.spec.js', '**/*.feature'],
