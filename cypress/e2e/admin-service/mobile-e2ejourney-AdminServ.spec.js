2b159dd57cff2766b994d71de5852a49a4d0a83b
MDola <madhu.dola@digital.education.gov.uk>
2022-11-28 19:36:53 +0000

    Modify Smoke test + mobile tests

--- cypress/e2e/admin-service/mobile-e2ejourney-AdminServ.spec.js
+++ cypress/e2e/admin-service/mobile-e2ejourney-AdminServ.spec.js
@@ -11,7 +11,8 @@ describe('| mobile-e2ejourney-AdminServ.spec | Manage Service - Create and delet
         // give service name
         cy.addService('TestService' + n)
         // select required service(s)
-        cy.selectSupport({supportwithparenting:'005b3184-6ffb-414a-a1e3-6d5674dc0e63',health:'32712b43-e4f7-484f-97d7-beb3bb463133'})
+         cy.serviceType1({health:'32712b43-e4f7-484f-97d7-beb3bb463133',familysupport:'94f0ba86-d5fb-4fac-a1ee-f12ba4ef3012'})
+        cy.serviceType2({hearingandsight:'11696b1f-209a-47b1-9ef5-c588a14d43c6',supportwithparenting:'005b3184-6ffb-414a-a1e3-6d5674dc0e63'})
         //type of service
         cy.serviceDeliveryType({online:'2', telephone:'3'})
         // who is it for ?
@@ -25,7 +26,7 @@ describe('| mobile-e2ejourney-AdminServ.spec | Manage Service - Create and delet
         //more details
         cy.moreDetails('Test details')
         // check details
-        cy.checkDetails('TestService' + n,'Health')
+        cy.checkDetails('TestService' + n,'Hearing and sight')
         // service added 
         cy.serviceAdded()
         // validate added service is present in list
