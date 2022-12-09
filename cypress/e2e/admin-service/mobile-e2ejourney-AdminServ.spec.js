bbd38cc353f35ee829c1897c348d6af82b826569
mdolaDFE <madhu.dola@digital.education.gov.uk>
2022-12-09 11:34:00 +0000

    Refactor regression - admin-ui

--- cypress/e2e/admin-service/mobile-e2ejourney-AdminServ.spec.js
+++ cypress/e2e/admin-service/mobile-e2ejourney-AdminServ.spec.js
@@ -9,15 +9,15 @@ describe('| mobile-e2ejourney-AdminServ.spec | Manage Service - Create and delet
         //sign in page
         cy.signInPage()
         // choose organisation
-        cy.chooseOrganisation('Bristol County Council')
+        cy.chooseOrganisation('Salford City Council')
         const num = Date.now();
         const n = num.toString();
         // select add service
-        cy.welcomePage('add')
+        cy.welcomePage('add','Salford City Council')
         // give service name
         cy.addService('TestService' + n)
         // select required service(s)
-         cy.serviceType1({health:'32712b43-e4f7-484f-97d7-beb3bb463133',familysupport:'94f0ba86-d5fb-4fac-a1ee-f12ba4ef3012'})
+        cy.serviceType1({health:'32712b43-e4f7-484f-97d7-beb3bb463133',familysupport:'94f0ba86-d5fb-4fac-a1ee-f12ba4ef3012'})
         cy.serviceType2({hearingandsight:'11696b1f-209a-47b1-9ef5-c588a14d43c6',supportwithparenting:'005b3184-6ffb-414a-a1e3-6d5674dc0e63'})
         //type of service
         cy.serviceDeliveryType({online:'2', telephone:'3'})
@@ -39,9 +39,17 @@ describe('| mobile-e2ejourney-AdminServ.spec | Manage Service - Create and delet
         cy.ViewServices('TestService' + n,)
         // manage service 
       // cy.ViewServices('TestService' + n)
+       cy.editService('testservice' + n)
+       // save without making any changes
+       cy.saveDetails()
+       // validate added service is present in list
+        cy.ViewServices('TestService' + n,)
+        // manage service 
+      // cy.ViewServices('TestService' + n)
        cy.deleteService('testservice' + n)
        // confirm deletion 
        cy.deleteConfirm('Yes')
+      
         
     })
 }
