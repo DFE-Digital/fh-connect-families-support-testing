a3a271e0e193134be4ad5ab0f0c25cd055bbd127
Harsha Reddy <harsha.reddy@education.gov.uk>
2023-08-16 21:57:17 +0100

    enabling the `experimentalSessionAndOrigin` flag

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   e2e: {
-       'baseUrl': 'https://test.connect-families-to-support.education.gov.uk',
+      'baseUrl': 'https://test.connect-families-to-support.education.gov.uk',
+      experimentalSessionAndOrigin : true,
 
     'specPattern': ['**/*.spec.js', '**/*.feature','**/*.cy'],
     setupNodeEvents(on, config) {
